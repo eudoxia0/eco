f0f1de39124bd6ff4d889a6d51278673604bb609
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-30 19:02:13 -0300

    Expand parser code

--- src/parser.lisp
+++ src/parser.lisp
@@ -2,3 +2,39 @@
 (defpackage eco.parser
   (:use :cl :esrap))
 (in-package :eco.parser)
+
+;;; Utility rules
+
+(defparameter +whitespace-chars+
+  (list #\Space #\Tab #\Newline #\Linefeed #\Backspace
+        #\Page #\Return #\Rubout))
+
+(defrule ws (+ (or #\Space #\Tab #\Newline #\Linefeed #\Backspace
+                   #\Page #\Return #\Rubout))
+  (:constant nil))
+
+;;; Expression tags
+
+(defrule expr-tag-char (not "%>"))
+
+(defrule expr-tag ("<%" (* ws) (* expr-tag-char) (* ws) "%>")
+  (:destructure (open ws1 text ws2 close)
+    (text text)))
+
+;;; Block tags
+
+(defrule block-tag-char (not "%}"))
+
+(defmacro nocontent-tag (name)
+  "Return an esrap matcher for a content-free tag of a given name."
+  `("{%" (* ws) ,name (* ws) "%}"))
+
+(defmacro content-tag (name)
+  "Return an esrap matcher for a tag of a given name."
+  `("{%" (* ws) ,name (+ ws) (* block-tag-char) (* ws) "%}"))
+
+(defmacro define-block (rule-name (&rest rules)
+                                  (&rest args)
+                                  &rest destructure)
+  `(defrule ,rule-name ,rules
+     (:destructure ,args ,destructure)))
