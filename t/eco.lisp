d1c1cd2763bbfa514903979bb1b8217bece9f0cd
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-30 20:02:02 -0300

    Add a test

--- t/eco.lisp
+++ t/eco.lisp
@@ -1,4 +1,14 @@
 (in-package :cl-user)
 (defpackage eco-test
-  (:use :cl :eco :fiveam))
+  (:use :cl :eco.parser :fiveam))
 (in-package :eco-test)
+
+(def-suite parser
+  :description "Testing the parser.")
+(in-suite parser)
+
+(test expressions
+  (is-true (typep (parse-template "<%%>")
+                  '<expr-tag>)))
+
+(run! 'parser)
