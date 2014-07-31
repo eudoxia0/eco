3f847d29919f882095e6f7c0139036d4395db0ba
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-31 16:40:01 -0300

    Add emit method for expression tags

--- src/compiler.lisp
+++ src/compiler.lisp
@@ -2,3 +2,6 @@
 (defpackage eco.compiler
   (:use :cl :eco.parser))
 (in-package :eco.compiler)
+
+(defmethod emit ((tag <expr-tag>))
+  (format nil "(format *eco-stream* \"~~A\" (~A))" (content tag)))
