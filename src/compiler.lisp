c268bc9779a843dbddc847503dbc6b0c855fc142
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-01 20:35:07 -0300

    Update compiler, add template definition

--- src/compiler.lisp
+++ src/compiler.lisp
@@ -3,13 +3,30 @@
   (:use :cl :eco.parser))
 (in-package :eco.compiler)
 
+(defmethod emit ((str string)) str)
+
 (defmethod emit ((tag <expr-tag>))
   (format nil "(format *eco-stream* \"~~A\" (~A))" (content tag)))
 
+(defun emit-list (list)
+  (mapcar #'(lambda (elem) (emit elem)) list))
+
+(defparameter +template-def+
+"(defun ~A (~A)
+  (with-output-to-string (*eco-parser*) ~{~A ~}))")
+
+(defun emit-template-definition (name arg-text body)
+  (format nil +template-def+ name arg-text (emit-list body)))
+
 (defmethod emit ((block <block>))
-  (format nil "(format *eco-stream* \"~~A\" (~A ~A ~{~A ~}))"
-          (name block)
-          (content block)
-          (map #'(lambda (elem)
-                   (emit elem))
-               (body block))))
+  (if (equal (name block) "template")
+      (let* ((text (content block))
+             (template-name (subseq text
+                                    0
+                                    (position #\Space text)))
+             (arg-text (subseq text (1+ (position #\Space text)))))
+        (emit-template-definition template-name arg-text (body block)))
+      (format nil "(format *eco-stream* \"~~A\" (~A ~A ~{~A ~}))"
+              (name block)
+              (content block)
+              (emit-list (body block)))))
