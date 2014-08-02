1de8817fc8cf712c7933491e34ceb89ae19a3170
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-01 21:25:53 -0300

    Add some pretty-printing code, delete other old code

--- src/parser.lisp
+++ src/parser.lisp
@@ -36,67 +36,15 @@
 
 (defrule expression (or statement raw-text))
 
-#|
-
 ;;; Pretty-printing
 
-(defmethod print-object ((tag <expr-tag>) stream)
-  (format stream "<% ~A%>" (content tag)))
-
-(defmethod print-object ((tag <content-tag>) stream)
-  (format stream "{% ~A ~A%}" (name tag) (content tag)))
-
-(defmethod print-object ((tag <end-tag>) stream)
-  (format stream "{% end~A %}" (name tag)))
-
 (defmethod print-object ((block <block>) stream)
-  (format stream "{% ~A ~A%}~&~{~A~&~}{% end~A%}"
-          (name block) (content block) (body block)
-          (name block)))
+  (format stream "{~&~A~&}" (body block)))
 
-;;; Utility rules
+(defmethod print-object ((statement <statement>) stream)
+  (format stream "@~A~{~A~}" (code statement) (body statement)))
 
-(defparameter +whitespace-chars+
-  (list #\Space #\Tab #\Newline #\Linefeed #\Backspace
-        #\Page #\Return #\Rubout))
-
-(defrule ws (+ (or #\Space #\Tab #\Newline #\Linefeed #\Backspace
-                   #\Page #\Return #\Rubout))
-  (:constant nil))
-
-;;; Expression tags
-
-(defrule expr-tag-char (not "%>"))
-
-(defrule expr-tag (and "<%" (* ws) (* expr-tag-char) (* ws) "%>")
-  (:destructure (open ws1 text ws2 close)
-    (make-instance '<expr-tag> :content (text text))))
-
-;;; Block tags
-
-(defrule tag-name (+ (not (or "%}" ws))))
-
-(defrule tag-text (* (not "%}")))
-
-(defrule content-tag (and "{%" (* ws) tag-name (+ ws) tag-text (* ws) "%}")
-  (:destructure (open ws1 name ws2 content ws3 close)
-    (make-instance '<content-tag>
-                   :name (text name)
-                   :content (text content))))
-
-(defrule end-tag (and "{%" (* ws) "end" tag-name (* ws) "%}")
-  (:destructure (open ws1 end text ws2 close)
-    (make-instance '<end-tag>
-                   :name (text text))))
-
-(defrule body-block (+ (not (and (or "<" "{") "%")))
-  (:destructure (&rest text)
-    (text text)))
-
-(defrule tag (or expr-tag end-tag content-tag))
-
-(defrule expression (* (or tag body-block)))
-
+#|
 (defun contentp (tok)
   (typep tok '<content-tag>))
 
