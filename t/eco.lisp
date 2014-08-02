2d8a0f13157bf2c1659558af7146f08332dbb3ef
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-02 11:29:28 -0300

    Tear down old tests, add a couple of new ones

--- t/eco.lisp
+++ t/eco.lisp
@@ -7,43 +7,8 @@
   :description "Testing the parser.")
 (in-suite parser)
 
-(test expression-tag
-  (is-true (typep (parse-template "<%%>")
-                  '<expr-tag>))
-  (is-true (typep (parse-template "<% a b c %>")
-                  '<expr-tag>))
-  (is (equal (content (parse-template "<% a b c %>"))
-             "a b c ")))
-
-(test content-tag
-  (is-true (typep (esrap:parse 'eco.parser::content-tag "{% a b c %}")
-                  '<content-tag>))
-  (is-true
-   (let* ((parsed (esrap:parse 'eco.parser::content-tag "{% test 1 2 3 %}"))
-          (name (name parsed))
-          (content (content parsed)))
-     (and (equal name "test")
-          (equal content "1 2 3 ")))))
-
-(test end-tag
-  (is-true (typep (esrap:parse 'eco.parser::end-tag "{% endtest %}")
-                  '<end-tag>))
-  (is (equal (name (esrap:parse 'eco.parser::end-tag "{% endtest %}"))
-             "test")))
-
-(test block
-  (is-true (typep (parse-template "{% block %} body {% endblock %}")
-                  '<block>)))
-
-(def-suite compiler
-  :description "Testing the compiler.")
-(in-suite compiler)
-
-(test expr-tag
-  (is (equal (eco.compiler::emit (eco.parser:parse-template "<%%>"))
-             "(format *eco-stream* \"~A\" ())"))
-  (is (equal (eco.compiler::emit (eco.parser:parse-template "<% f 1 2 3 %>"))
-             "(format *eco-stream* \"~A\" (f 1 2 3 ))")))
+(test parsing
+  (is-true (typep (parse-template "232323") 'string))
+  (is-true (typep (parse-template "@derp{1 2 3}") '<statement>)))
 
 (run! 'parser)
-(run! 'compiler)
