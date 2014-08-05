59f3c0fbe175eb4fe3c62364d3bbb0b5a52cd522
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-05 16:04:58 -0300

    Expand tests

--- t/eco.lisp
+++ t/eco.lisp
@@ -7,8 +7,14 @@
   :description "Testing the parser.")
 (in-suite parser)
 
+(test rules
+  (is-true (typep (esrap:parse 'eco.parser::block "{1 2 3}") '<block>))
+  (is-true (typep (esrap:parse 'eco.parser::block "{1 2 3}   ") '<block>)))
+
 (test parsing
+  (is-true (typep (parse-template "") 'string))
   (is-true (typep (parse-template "232323") 'string))
-  (is-true (typep (parse-template "@derp{1 2 3}") '<statement>)))
+  (is-true (typep (parse-template "@derp{1 2 3}") '<statement>))
+  (is-true (typep (parse-template "@derp{a}{b}") '<statement>)))
 
 (run! 'parser)
